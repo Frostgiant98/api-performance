1b036aedc44253a4a6c13ed74aab3d094104dbb3
Frostgiant98 <niceice98@gmail.com>
2022-07-30 16:06:13 -0400

    created model db

--- core/courses/models.py
+++ core/courses/models.py
@@ -1,3 +1,17 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=99)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    title = models.CharField(max_length=99)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return super(self.title).__str__()
